5622f5d0734d526ba5fe0fbe90895fd2f43cc645
baholdin <al.bahol@yandex.ru>
2020-05-29 12:05:29 +0300

    REGRES before add split category

---
+++ GameStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+
+	}
+}
